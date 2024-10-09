22fa5c3e8268456daf085a00f8c5fa656858c07c
AdithyaaAbhi <131256294+AdithyaaAbhi@users.noreply.github.com>
2024-10-09 16:31:40 +0530

    Update test_string_calculator.py

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,10 +1,10 @@
 import unittest
-from string_calculator import add
+from string_calculator import add  # Importing the add function from string_calculator.py
 
 class TestStringCalculator(unittest.TestCase):
+    
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
 
 if __name__ == "__main__":
     unittest.main()
-  
